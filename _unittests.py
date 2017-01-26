5c5cd769b6eb36bac2c8c805566e0d45eb4ab4a6
v.kokorev <v.kokorev@utwente.nl>
2017-01-26 13:09:03 +0100

    placeholder for unittests added

---
+++ _unittests.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# coding=utf-8
+"""
+here be unittests
+"""
+import unittest
+
+
+class test_netCDFReader(unittest.TestCase):
+
+    def setUp(self):
+        from readers import netcdfReader
+        y_config = {
+            'fn': r'C:\data\SACA&D\grid\rr_0.5deg_regular_mon_anoms.nc',
+            'var_name': 'rr',
+            'lat_name': 'latitude',
+            'lon_name': 'longitude',
+            'reader': netcdfReader,
+        }
+        self.reader = netcdfReader(**y_config)
+        self.reader.set_time_masks(1991, 1993)
+
+    def test_get_predictor(self):
+        self.assertAlmostEqual(self.reader.get_predictor(9, 75, 1)[0], -22.3, places=2) # -105.9, -58.3, np.nan, -64.1
+        self.assertAlmostEqual(self.reader.get_predictor(9, 75, 1)[1], -105.9, places=2)
+        self.assertAlmostEqual(self.reader.get_predictor(9, 75, 1)[2], -58.3, places=2)
+
+    def tearDown(self):
+        self.reader.nc.close()
